dec0163802d63e261a27493303df192bcd31e911
Dreamboat <enechimdindu@gmail.com>
2024-12-01 08:25:08 +0100

    registering the book model with django admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+
+class Bookadmin(admin.ModelAdmin):
+    list_Book =  ('title', 'author', 'publication_year')
+    search_app = ('title', 'author')
+
+admin.site.register(Book)

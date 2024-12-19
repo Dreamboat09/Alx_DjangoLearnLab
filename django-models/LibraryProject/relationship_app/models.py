33a1d12823d92b4164f0acaf27833834a7316f93
Dreamboat <enechimdindu@gmail.com>
2024-12-19 08:35:41 +0100

    setting up models of  a library app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,15 +5,30 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=20)
     
+    def __str__(self):
+        return self.name
+    
+    
 class Book(models.Model):
     title = models.CharField(max_length=20)
     author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
     
 class Library(models.Model):
     name = models.CharField(max_length=20)
     books = models.ManyToManyField(Book)
     
+    def __str__(self):
+        return self.name
+    
+    
 class Librarian(models.Model):
     name = models.CharField(max_length=20)
     library = models.OneToOneField(Library,on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
     

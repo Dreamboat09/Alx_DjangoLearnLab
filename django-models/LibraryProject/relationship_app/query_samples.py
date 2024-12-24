43627859427729f3e97a4d7529e4999339222066
Dreamboat <enechimdindu@gmail.com>
2024-12-24 09:14:12 +0100

    setting up models of  a library app

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,28 +1,47 @@
 import mysql.connector
-import .models
+from .models import Book, Author, Library, Librarian
 
 try:
     mydb = mysql.connector.connect(
         host = 'localhost'
         user = 'root'
         passwd = 'dreamboat'
-    )
+        database = 'bookDB'
+        )
     
     mycursor = mydb.cursor
     
-    findbyauthor = Book.objects.filter(Author)
+    def book_by_author(author_name):
+        try:
+            authors = Author.objects.get(author_name)
+            books = Book.objects.filter(Author=authors)
+            return books
+        except books.DoesNotExist:
+            return[]
+            
+    def book_in_a_library(library_name):
+        try:
+            Librarys = Library.objects.get(library_name)
+            books = Book.objects.filter(Library=Librarys)
+            return books
+        except books.DoesNotExist:
+            return []
+        
+    def libarian_by_library(library_name):
+        try:
+            Librarys = Library.objects.get(library_name)
+            Librarians = Librarian.objects.filter(Library=Librarys)
+            return Librarians
+        except Librarians.DoesNotExist:
+            return []
+            
     
-    allbook = Book.object.all
-    
-    librarian =  Library.object(Librarian)
-    
-    
-except: 
+except:
     mydb = mysql.connector.error
     print('error')
     
 finally:
-    if mydb = mysql.connector.connect:
+    if mydb = mysql.connector.connect
         mycursor.close()
         mydb.close
     
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book Author Library Librarian
+
+
+def listbook(request):
+    book = Book.object.all
+    return render (request, ) 

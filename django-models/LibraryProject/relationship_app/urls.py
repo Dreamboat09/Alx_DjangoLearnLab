bce6435002e95f9ddd7e4d3845be6bda93e422ec
Dreamboat <enechimdindu@gmail.com>
2025-01-07 00:29:44 +0100

    authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from .views import list_books, LibraryDetailView, userlogin, userlogout, register
-
+from . import views
 urlpatterns = [
     path('accounts/', include([
         path('login/', userlogin.as_view(), name='login'),
         path('logout/', userlogout.as_view(), name='logout'),
-        path('register/', register, name='register'),
+        path('register/', views.register, name='register'),
         ])),
     path('book/', list_books, name='book'),
     path('library/', LibraryDetailView.as_view(), name='library'),

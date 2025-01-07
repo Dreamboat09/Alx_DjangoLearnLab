251c38ca65f19298f36c16ba8d3ee7d6bccbd1f0
Dreamboat <enechimdindu@gmail.com>
2025-01-07 16:28:41 +0100

    RBAC sys

--- django-models/LibraryProject/relationship_app/user_views/admin_view.py
+++ django-models/LibraryProject/relationship_app/user_views/admin_view.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 def is_admin(user):
-    return user.role == "Admin"
+    return user.userprofile.role == "Admin"
 
 @user_passes_test(is_admin)
 def admin_view(request):

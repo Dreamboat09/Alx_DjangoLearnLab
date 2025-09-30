21369a65489250d0fc09a8242f91b1e475babd94
Dreamboat09 <enechimdindu@gmail.com>
2025-09-30 01:45:07 +0100

    Add login, logout, and register templates; update URLs and views for authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, list_books
+from .views import list_books, LibraryDetailView, register
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('library', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -15,3 +15,20 @@ from .models import Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+
+
+
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('book_list')  # Redirect to a success page.
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})

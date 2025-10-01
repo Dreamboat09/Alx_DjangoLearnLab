3196a4834157d63f65bdda3475db59dd3e3f2b08
Dreamboat09 <enechimdindu@gmail.com>
2025-10-01 21:54:17 +0100

    Add URL pattern for editing a book

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('change_book/<int:book_id>/', views.change_book, name='change_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # New URL pattern for editing a book
 ]

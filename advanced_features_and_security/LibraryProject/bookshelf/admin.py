e6f48bf23f22138f2f6c64621a984798cecd2de0
Dreamboat09 <enechimdindu@gmail.com>
2025-10-02 23:34:39 +0100

    implemting a custom user model and it manager

---
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Book, CustomUser
+
+# Register your models here.
+#class BookAdmin(admin.ModelAdmin):
+#    list_display = ('title', 'author', 'published_date')
+#    search_fields = ('title', 'author')
+#    list_filter = ('published_date',)
+
+#admin.site.register(Book, BookAdmin)
+
+
+class custom_ModelAdmin(admin.ModelAdmin):
+    list_display = ('email', 'password', 'date_of_birth', 'profile_photo')
+    search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ('date_joined', 'last_login')
+
+admin.site.register(CustomUser, custom_ModelAdmin)
---
+++ advanced_features_and_security/LibraryProject/bookshelf/retrieve.md
@@ -0,0 +1,9 @@
+from bookshelf.models import Book
+
+books = Book.objects.get(title="1984")
+
+# getting the result to the standard output
+
+print(book)
+
+<QuerySet [<Book: 1984>]>

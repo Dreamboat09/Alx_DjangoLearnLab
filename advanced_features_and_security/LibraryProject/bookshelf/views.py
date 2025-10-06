88762f1af1253c632039dc80fc2745deb3f9f3aa
Dreamboat09 <enechimdindu@gmail.com>
2025-10-06 21:40:38 +0100

    Implement custom user model and add book management features with user and group permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
 from django.contrib.auth.decorators import permission_required
 
 # Create your views here.
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def view_books(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'bookshelf/view_books.html', context)
+    return redirect('book_list')
 
 @permission_required('bookshelf.can_create', raise_exception=True)
-def add_book(request, books):
+def add_book(request):
     if request.method == 'POST':
         books = Book.objects.create(
             title=request.POST['title'],
-            author=request.POST['author'],x
-            published_date=request.POST['published_date']
+            author=request.POST['author'],
+            published_date=request.POST['published_date'],
         )
         books.save()
-        return render(request, 'bookshelf/view_books.html', {'books': Book.objects.all()})
-    return render(request, 'bookshelf/add_book.html')
+        return redirect ('book_list')
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, book_id):
@@ -30,13 +30,12 @@ def edit_book(request, book_id):
         book.author = request.POST['author']
         book.published_date = request.POST['published_date']
         book.save()
-        return render(request, 'bookshelf/view_books.html', {'books': Book.objects.all()})
-    return render(request, 'bookshelf/edit_book.html', {'book': book})
+    return redirect('book_list')
 
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):
     book = Book.objects.get(id=book_id)
     book.delete()
-    return render(request, 'bookshelf/view_books.html', {'books': Book.objects.all()})
+    return redirect('book_list')    
 
 

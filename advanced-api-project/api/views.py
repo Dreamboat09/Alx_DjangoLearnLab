6831b31dc3a81bd10c3b82240bb49c837c5d8461
Dreamboat09 <enechimdindu@gmail.com>
2025-10-16 02:46:45 +0100

    Implement filtering, searching, and ordering for books: add DjangoFilterBackend and update BookListView

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from .models import Book, Author
 from .serializers import BookSerializer, AuthorSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, IsAdminUser
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+from rest_framework import generics
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 

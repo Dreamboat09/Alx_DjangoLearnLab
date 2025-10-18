75ee69af6c0be52ca884ea27aac38c431cd5c675
Dreamboat09 <enechimdindu@gmail.com>
2025-10-18 12:07:58 +0100

    change database default from sqlite to mysql

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BLOG',
+        'USER': 'alx',
+        'PASSWORD': 'alx12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
